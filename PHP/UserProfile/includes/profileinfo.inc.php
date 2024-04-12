6d1ea10226f26bee7940418a58fb55f141ce3b0a
Man Njoro <elijohnmwoho@gmail.com>
2024-04-12 23:45:56 +0300

    profile settings

---
+++ PHP/UserProfile/includes/profileinfo.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_SESSION["user_id"];
+    $username = $_SESSION["user_username"];
+    $about = htmlspecialchars($_POST["about"], ENT_QUOTES, "UTF-8");
+    $introTitle = htmlspecialchars($_POST["introtitle"], ENT_QUOTES, "UTF-8");
+    $introText = htmlspecialchars($_POST["introtext"], ENT_QUOTES, "UTF-8");
+    include "./dbh.inc.php";
+    include "../models/ProfileInfoModel.php";
+    include "../controllers/ProfileInfoController.php";
+    include "../views/ProfileView.php";
+
+    $profileInfo = new ProfileInfoController($id, $username);
+
+    $profileInfo->updateProfileInfo($about, $introTitle, $introText);
+    header("Location: ../profile.php?error=none");
+}
