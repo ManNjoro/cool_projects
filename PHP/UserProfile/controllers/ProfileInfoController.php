89213dd49d93cb1ea0e481d1b9a61df457b1b911
Man Njoro <elijohnmwoho@gmail.com>
2024-04-12 20:41:43 +0300

    Add CSS for user profile view and create ProfileInfoView class
    The commit adds CSS for the user profile page, including a new .profile class and related styles. It also creates a ProfileInfoView class in the PHP code. However, the

---
+++ PHP/UserProfile/controllers/ProfileInfoController.php
@@ -0,0 +1,5 @@
+<?php
+
+class ProfileInfoController extends ProfileInfo {
+    
+}
---
+++ PHP/UserProfile/myfirstdatabase.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS myfirstdatabase;
+USE myfirstdatabase;
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+SELECT *
+FROM users;
+CREATE TABLE IF NOT EXISTS profiles (
+    profiles_id int NOT NULL AUTO_INCREMENT,
+    profiles_about TEXT NOT NULL,
+    profiles_introtitle TEXT NOT NULL,
+    profiles_introtext TEXT NOT NULL,
+    users_id int,
+    PRIMARY KEY (profiles_id),
+    FOREIGN KEY (users_id) REFERENCES users(users_id)
+);
