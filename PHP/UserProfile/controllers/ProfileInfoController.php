4ad2fabe5890d9d0e40352143ec08092d7bbfd2b
Man Njoro <elijohnmwoho@gmail.com>
2024-04-12 21:21:12 +0300

    Update ProfileInfoController and ProfileInfoModel classes
    - ProfileInfoController: added constructor with userId and username parameters, and a defaultProfileInfo method
    - ProfileInfoModel: added methods getProfileInfo, setNewProfileInfo, and setProfile

--- PHP/UserProfile/controllers/ProfileInfoController.php
+++ PHP/UserProfile/controllers/ProfileInfoController.php
@@ -1,5 +1,21 @@
 <?php
 
-class ProfileInfoController extends ProfileInfo {
-    
+class ProfileInfoController extends ProfileInfo
+{
+    private $userId;
+    private $username;
+
+    public function __construct($userId, $username)
+    {
+        $this->userId = $userId;
+        $this->username = $username;
+    }
+
+    public function defaultProfileInfo()
+    {
+        $profileAbout = "Tell people about yourself! Your interests, hobbies, or favorite TV show!";
+        $profileTitle = "Hi! I am " . $this->username;
+        $profileText = "Welcome to my profile page! Soon I'll be able to tell you more about myself, and what you can find on my profile page.";
+        $this->setProfileInfo($profileAbout, $profileTitle, $profileText, $this->userId);
+    }
 }
--- PHP/UserProfile/models/ProfileInfoModel.php
+++ PHP/UserProfile/models/ProfileInfoModel.php
@@ -1,5 +1,58 @@
 <?php
 
-class ProfileInfo extends Dbh {
-    
+class ProfileInfo extends Dbh
+{
+    protected function getProfileInfo($userId)
+    {
+        $query = "SELECT * FROM profiles WHERE users_id=:users_id;";
+        $stmt = parent::connect()->prepare($query);
+        $stmt->bindParam(":users_id", $userId);
+        if (!$stmt->execute()) {
+            $stmt = null;
+            header("location: ../profile.php?error=stmtfailed");
+            exit();
+        }
+
+        if ($stmt->rowCount() == 0) {
+            $stmt = null;
+            header("Location: ../profile.php?error=profilenotfound");
+            exit();
+        }
+
+        $profileData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = null;
+        return $profileData;
+    }
+
+    protected function setNewProfileInfo($profileAbout, $profileTitle, $profileText, $userId)
+    {
+        $query = "UPDATE profiles SET profiles_about = :profiles_about, profiles_introtitle=:profiles_introtitle, profiles_introtext=:profiles_introtext WHERE users_id=:users_id;";
+        $stmt = parent::connect()->prepare($query);
+        $stmt->bindParam(":profiles_about", $profileAbout);
+        $stmt->bindParam(":profiles_introtitle", $profileTitle);
+        $stmt->bindParam(":profiles_introtext", $profileText);
+        $stmt->bindParam(":users_id", $userId);
+        if (!$stmt->execute()) {
+            $stmt = null;
+            header("location: ../profile.php?error=stmtfailed");
+            exit();
+        }
+        $stmt = null;
+    }
+
+    protected function setProfileInfo($profileAbout, $profileTitle, $profileText, $userId)
+    {
+        $query = "INSERT INTO profiles (profiles_about, profiles_introtitle, profiles_introtext, users_id) VALUES (:profiles_about, :profiles_introtitle, :profiles_introtext, :users_id);";
+        $stmt = parent::connect()->prepare($query);
+        $stmt->bindParam(":profiles_about", $profileAbout);
+        $stmt->bindParam(":profiles_introtitle", $profileTitle);
+        $stmt->bindParam(":profiles_introtext", $profileText);
+        $stmt->bindParam(":users_id", $userId);
+        if (!$stmt->execute()) {
+            $stmt = null;
+            header("location: ../profile.php?error=stmtfailed");
+            exit();
+        }
+        $stmt = null;
+    }
 }
