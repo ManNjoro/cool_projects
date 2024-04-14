f7e554bb89cc4c683f7a2cb88c1171001767fa02
Man Njoro <elijohnmwoho@gmail.com>
2024-04-14 22:19:03 +0300

    "Added enable multi selection feature in accordion component"
    This commit adds the ability to enable multi-selection mode in the accordion component. A new state variable `enableMultiSelection` is introduced to control this mode. When enabled, multiple

--- React/accordion/src/components/Accordion.jsx
+++ React/accordion/src/components/Accordion.jsx
@@ -1,46 +1,71 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Accordion() {
-    const [selected, setSelected] = useState(null)
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
+  const [selected, setSelected] = useState(null);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-    useEffect(()=>{
-        axios.get('http://localhost:8000/data')
-        .then(res => setData(res.data))
-        .catch(e => console.log(e.message))
-        .finally(()=> setLoading(false))
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/data")
+      .then((res) => setData(res.data))
+      .catch((e) => console.log(e.message))
+      .finally(() => setLoading(false));
+  }, []);
 
-    const handleSingleSelection = (id) => {
-        setSelected(id === selected ? null : id);
-    }
-    
+  const handleSingleSelection = (id) => {
+    setSelected(id === selected ? null : id);
+  };
+
+  const handleMultiSelection = (id) => {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(id);
+    if (findIndexOfCurrentId == -1) cpyMultiple.push(id);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(cpyMultiple);
+  };
+  console.log(selected, multiple, enableMultiSelection);
+
   return (
-    <div className='wrapper'>
-        <div className="accordion">
-            {
-                data && data.length > 0 ?
-                data.map((dataItem => (
-                    <div className="item" key={dataItem.id}>
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
-                        </div>
-                        {
-                            selected === dataItem.id ? 
-                            <div className="content">{dataItem.answer}</div>
-                            :
-                            null
-                        }
-                    </div>
-                )))
-                :
-                <div>No data found</div>
-            }
-        </div>
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {/* {
+                enableMultiSelection ? multiple.indexOf(dataItem.id) !== 1 && (
+                  <div className="content">{dataItem.answer}</div>
+                ) : selected === dataItem.id &&
+                  <div className="content">{dataItem.answer}</div>
+              } */}
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
