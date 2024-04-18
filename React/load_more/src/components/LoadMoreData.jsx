1029efbdfff1ab4613ea499eefe3fbe8e70dffa1
Man Njoro <elijohnmwoho@gmail.com>
2024-04-18 14:19:51 +0300

    "Implement load more functionality with pagination, add loading state, and disable button when reaching max products"
    This commit includes the following changes:
    
    1. Updated `LoadMoreData` component to implement load more functionality with pagination,

--- React/load_more/src/components/LoadMoreData.jsx
+++ React/load_more/src/components/LoadMoreData.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
@@ -6,19 +7,58 @@ export default function LoadMoreData() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
   const fetchProducts = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
-        `https://dummyjson.com/products?limit=10&skip=${count === 0 ? 0 : count * 20}`
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
       );
       console.log(response.data);
-      setProducts(response.data.products)
+      const result = response.data.products;
+
+      if (response.data && result && result.length) {
+        setProducts((prevData) => [...prevData, ...result]);
+      }
     } catch (e) {
       console.log(e);
+    } finally {
+      setLoading(false);
     }
   };
 
-  useEffect(() => {fetchProducts()}, []);
-  return <div></div>;
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    products && products.length === 100 && setDisableButton(true);
+  }, [products]);
+  return (
+    <>
+      {loading && <div>Loading data! Please wait.</div>}
+      <div className="container">
+        <div className="product-container">
+          {products && products.length
+            ? products.map((item) => (
+                <div className="product" key={item.id}>
+                  <img src={item.thumbnail} alt={item.title} />
+                  <p>{item.title}</p>
+                </div>
+              ))
+            : null}
+        </div>
+        <div className="button-container">
+          <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+            Load More Products
+          </button>
+          {disableButton && <p>You have reached to 100 products. The max!</p>}
+        </div>
+      </div>
+      ;
+    </>
+  );
 }
