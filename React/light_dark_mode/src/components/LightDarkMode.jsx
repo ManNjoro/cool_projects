839d7ff1496928c9b090ccdb93fd7f1662cfb7ac
Man Njoro <elijohnmwoho@gmail.com>
2024-04-20 17:46:42 +0300

    "Remove default starter code in App.jsx"
    Explanation:
    The code diff shows the removal of the default starter code in App.jsx, including the import statements, the count state variable and its related button

---
+++ React/light_dark_mode/src/components/LightDarkMode.jsx
@@ -0,0 +1,16 @@
+import useLocalStorage from "./useLocalStorage";
+
+export default function LightDarkMode() {
+    const [theme, setTheme] = useLocalStorage('theme', 'dark')
+    const handleToggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
+  return (
+    <div className="light-dark-mode">
+      <div className="container">
+        <p>Hello World</p>
+        <button onClick={handleToggleTheme}>Change Theme</button>
+      </div>
+    </div>
+  );
+}
