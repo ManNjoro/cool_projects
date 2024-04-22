1ec3fee73ed39d420aec394496f8e377d9b413d1
Man Njoro <elijohnmwoho@gmail.com>
2024-04-22 19:39:55 +0300

    "Add props to Modal component and customize Modal content in ModalTest"
    This commit adds props to the Modal component, including `id`, `header`, `body`, `footer`, and `setShowModalPopup`.

--- React/modal/src/components/Modal.jsx
+++ React/modal/src/components/Modal.jsx
@@ -1,7 +1,37 @@
+/* eslint-disable react/prop-types */
 
-
-export default function Modal() {
+export default function Modal({ id, header, body, footer, setShowModalPopup }) {
+  const handleCloseModal = () => {
+    setShowModalPopup(false);
+  };
   return (
-    <div>Modal</div>
-  )
+    <div id={id || "Modal"} className="modal">
+      <div className="content">
+        <div className="header">
+          <span className="close-modal-icon" onClick={handleCloseModal}>
+            &times;
+          </span>
+          <h2>{header ? header : "Header"}</h2>
+        </div>
+        <div className="body">
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This is our Modal body</p>
+            </div>
+          )}
+        </div>
+        <div className="footer">
+          {footer ? (
+            footer
+          ) : (
+            <div>
+              <p>This is our footer</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- React/modal/src/components/ModalTest.jsx
+++ React/modal/src/components/ModalTest.jsx
@@ -12,7 +12,9 @@ export default function ModalTest() {
     <div>
       <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
       {
-        showModalPopup && <Modal />
+        showModalPopup && <Modal
+        body={<div>Customized body</div>}
+         setShowModalPopup={setShowModalPopup}/>
       }
     </div>
   )
