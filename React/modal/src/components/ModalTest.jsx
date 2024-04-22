d17e101bd2466620d1797ce0baecd7acfda98ef7
Man Njoro <elijohnmwoho@gmail.com>
2024-04-22 19:20:20 +0300

    "Added state and toggle function for modal popup; updated return statement to include button that opens the modal popup"

--- React/modal/src/components/ModalTest.jsx
+++ React/modal/src/components/ModalTest.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useState } from "react"
+
 
 export default function ModalTest() {
+  const [showModalPopup, setShowModalPopup] = useState(false)
+
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup)
+  }
   return (
-    <div>ModalTest</div>
+    <div>
+      <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
+    </div>
   )
 }
