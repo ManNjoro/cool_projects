cd53c6cc84f8eb20cbee0518e0b0290449b066c5
Man Njoro <elijohnmwoho@gmail.com>
2024-04-25 14:21:28 +0300

    Update User component and index.css
    - Remove bio, public\_repos, followers, and following from User component display
    - Add className to User component elements and update styles in index.css
    - Add container for user profile information and

--- React/github-profile-finder/src/components/User.jsx
+++ React/github-profile-finder/src/components/User.jsx
@@ -8,7 +8,6 @@ export default function User({ user }) {
     public_repos,
     name,
     login,
-    bio,
     created_at,
   } = user;
 
@@ -18,7 +17,7 @@ export default function User({ user }) {
       <div>
         <img src={avatar_url} alt="User" className="avatar" />
       </div>
-      <div>
+      <div className="name-container">
         <a href={`https://github.com/${login}`}>{name || login}</a>
         <p>
           User joined on{" "}
@@ -27,21 +26,20 @@ export default function User({ user }) {
           })} ${createdDate.getFullYear()}`}
         </p>
       </div>
-      <div>
-        <p>Bio</p>
-        <p>{bio}</p>
-      </div>
-      <div>
-        <p>Public Repos</p>
-        <p>{public_repos}</p>
-      </div>
-      <div>
-        <p>Followers</p>
-        <p>{followers}</p>
-      </div>
-      <div>
-        <p>Following</p>
-        <p>{following}</p>
+      <div className="profile-info">
+
+        <div>
+          <p>Public Repos</p>
+          <p>{public_repos}</p>
+        </div>
+        <div>
+          <p>Followers</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p>Following</p>
+          <p>{following}</p>
+        </div>
       </div>
     </div>
   );
