3a399212bcffd255beeb31eaaef108b4b40ef762
Man Njoro <elijohnmwoho@gmail.com>
2024-04-25 13:14:25 +0300

    "Add state for user name, search handler, and fetch user data on mount"
    This commit adds the ability to input a Github username and fetch user data when the search button is clicked or the username is changed. It sets up the

--- React/github-profile-finder/src/components/GithubProfileFinder.jsx
+++ React/github-profile-finder/src/components/GithubProfileFinder.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState();
+
+  function handleSubmit() {}
+
+  async function fetchGithubUserData() {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
   return (
-    <div>GithubProfileFinder</div>
-  )
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="search-by-username"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+    </div>
+  );
 }
