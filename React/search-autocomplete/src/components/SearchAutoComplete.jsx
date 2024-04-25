f2c213428c123b16001b016c80e53022cdce76e2
Man Njoro <elijohnmwoho@gmail.com>
2024-04-25 14:46:00 +0300

    "Replace app component with SearchAutoComplete component and implement data fetching with error handling"

--- React/search-autocomplete/src/components/SearchAutoComplete.jsx
+++ React/search-autocomplete/src/components/SearchAutoComplete.jsx
@@ -1,9 +1,46 @@
-
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function SearchAutoComplete() {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  async function fetchListOfUsers() {
+    try {
+      const res = await fetch("https://dummyjson.com/users");
+      const data = await res.json();
+      console.log(data);
+      if (data && data.users && data.users.length) {
+        setUsers(data.users);
+        setError(null);
+      }
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
-    <div className='search-autocomplete-container'>
-        <input type="text" name="search-users" placeholder="Search Users here..." />
+    <div className="search-autocomplete-container">
+      <input
+        type="text"
+        name="search-users"
+        placeholder="Search Users here..."
+      />
     </div>
-  )
+  );
 }
