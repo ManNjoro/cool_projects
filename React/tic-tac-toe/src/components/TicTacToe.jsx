f8126ffbc208031baa555e81b1242a538cd4e963
Man Njoro <elijohnmwoho@gmail.com>
2024-04-29 21:38:42 +0300

    Initial commit

---
+++ React/tic-tac-toe/src/components/TicTacToe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TicTacToe() {
+  return (
+    <div className='tic-tac-toe-container'>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+    </div>
+  )
+}
