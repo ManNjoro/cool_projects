cd717a91e4800b8add5eabd10b4feb3e2e27d713
Man Njoro <elijohnmwoho@gmail.com>
2024-05-09 16:47:47 +0300

    "Add list editable fields and register Profile model in admin site"
    This commit adds the ability to edit certain fields directly from the list view in the Django admin interface for the CustomUser and Message models. It also registers the Profile model

--- Django/chatapp/backend/api/admin.py
+++ Django/chatapp/backend/api/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Message, CustomUser
+from .models import Message, CustomUser, Profile
 
 # Register your models here.
 
-admin.site.register(Message)
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_editable = ['verified']
+    list_display = ['user','full_name', 'verified']
+
+class MessageAdmin(admin.ModelAdmin):
+    list_editable = ['is_read']
+    list_display = ['sender','receiver', 'content', 'is_read']
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
