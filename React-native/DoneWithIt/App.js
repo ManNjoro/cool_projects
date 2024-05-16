46aac11c3c67229e3cdc82fe0d5cb250bd0a54eb
Man Njoro <elijohnmwoho@gmail.com>
2024-05-16 14:23:52 +0300

    "Added flexWrap, flexBasis, and flexGrow styles to a View, and added two new Views with different background colors."

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -27,10 +27,14 @@ export default function App() {
       flex: 1,
       flexDirection: "row",
       justifyContent: "center",
-      alignItems:"center"
+      alignItems:"center",
+      alignContent: "center", // used with wrap
+      flexWrap: "wrap"
     }}>
       <View style={{
         backgroundColor:"dodgerblue",
+        flexBasis: 100, // width or height
+        flexGrow: 1,
         width: 100,
         height: 100,
         // alignSelf: "flex-start"
@@ -42,6 +46,16 @@ export default function App() {
       }}/>
       <View style={{
         backgroundColor:"tomato",
+        width: 100,
+        height: 100,
+      }}/>
+      <View style={{
+        backgroundColor:"grey",
+        width: 100,
+        height: 100,
+      }}/>
+      <View style={{
+        backgroundColor:"greenyellow",
         width: 100,
         height: 100,
       }}/>
