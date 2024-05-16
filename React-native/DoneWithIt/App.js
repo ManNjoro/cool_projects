cd1291a9943836d752bb2e68802f1b98091990c2
Man Njoro <elijohnmwoho@gmail.com>
2024-05-16 14:56:05 +0300

    "Refactored App.js: replaced original layout with new layout, added image component, and commented out old code"

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -15,56 +15,119 @@ import {
   View,
 } from "react-native";
 
-import { useDeviceOrientation } from "@react-native-community/hooks"
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
   // console.log("App executed");
   // console.log(Dimensions.get("screen"));
   // const orientation = useDeviceOrientation();
   return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems:"center",
-      alignContent: "center", // used with wrap
-      flexWrap: "wrap"
-    }}>
-      <View style={{
-        backgroundColor:"dodgerblue",
-        flexBasis: 100, // width or height
-        flexGrow: 1,
-        width: 100,
-        height: 100,
-        // alignSelf: "flex-start"
-      }}/>
-      <View style={{
-        backgroundColor:"gold",
-        width: 100,
-        height: 100,
-      }}/>
-      <View style={{
-        backgroundColor:"tomato",
-        width: 100,
-        height: 100,
-      }}/>
-      <View style={{
-        backgroundColor:"grey",
-        width: 100,
-        height: 100,
-      }}/>
-      <View style={{
-        backgroundColor:"greenyellow",
-        width: 100,
-        height: 100,
-      }}/>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+        
+      <View
+        style={{
+          backgroundColor: "black",
+          width: "100%",
+          height: "20%",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          padding: 20
+        }}
+      >
+        <View style={{
+          backgroundColor: "red",
+          width: 50,
+          height: 50
+        }} />
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 50,
+          height: 50
+        }} />
+      </View>
+      <Image
+          source={require("./assets/icon.png")}
+          style={{ width: "100%", height: "60%", objectFit: "contain" }}
+        />
+        <View
+        style={{
+          backgroundColor: "black",
+          width: "100%",
+          height: "20%",
+        }}
+      />
     </View>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //   }}
+    // >
+    //   <Image
+    //     source={require("./assets/icon.png")}
+    //     style={{ width: "100%", height: "80%", objectFit: "contain" }}
+    //   />
+    //   <View
+    //     style={{
+    //       backgroundColor: "red",
+    //       width: "100%",
+    //       height: "10%",
+    //     }}
+    //   />
+    //   <View
+    //     style={{
+    //       backgroundColor: "dodgerblue",
+    //       width: "100%",
+    //       height: "10%",
+    //     }}
+    //   />
+    // </View>
+    // <View style={{
+    //   backgroundColor: "#fff",
+    //   flex: 1,
+    //   flexDirection: "row",
+    //   justifyContent: "center",
+    //   alignItems:"center",
+    //   alignContent: "center", // used with wrap
+    //   flexWrap: "wrap"
+    // }}>
+    //   <View style={{
+    //     backgroundColor:"dodgerblue",
+    //     flexBasis: 100, // width or height
+    //     flexGrow: 1,
+    //     width: 100,
+    //     height: 100,
+    //     // alignSelf: "flex-start"
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor:"gold",
+    //     width: 100,
+    //     height: 100,
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor:"tomato",
+    //     width: 100,
+    //     height: 100,
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor:"grey",
+    //     width: 100,
+    //     height: 100,
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor:"greenyellow",
+    //     width: 100,
+    //     height: 100,
+    //   }}/>
+    // </View>
     // <View style={styles.container}>
     //   <View style={{
     //     backgroundColor: "dodgerblue",
     //     width: "100%",
-    //     height: orientation === "landscape" ? "100%":"30%", 
+    //     height: orientation === "landscape" ? "100%":"30%",
     //   }}></View>
     // </View>
 
@@ -109,14 +172,14 @@ export default function App() {
   );
 }
 
-const containerStyle = {backgroundColor: "dodgerblue"}
+const containerStyle = { backgroundColor: "dodgerblue" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
     // paddingTop: Platform.OS === "android" ? 20 : 0
   },
 });
