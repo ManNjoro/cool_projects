fcb3c662b64b94d63adb348ef2b5da6e645ea0e4
Man Njoro <elijohnmwoho@gmail.com>
2024-05-17 09:23:44 +0300

    "Modified ytDownloader.py: added progress and complete_func variables, updated YouTube constructor call"

--- python/youtubeDownloader/ytDownloader.py
+++ python/youtubeDownloader/ytDownloader.py
@@ -2,9 +2,11 @@ from pytube import YouTube
 from sys import argv
 path = '/d/Downloads'
 link = argv[1]
-yt = YouTube(link, on_progress_callback=progress_func,
+progress= 0
+progress_func = lambda :progress + 5
+complete_func = lambda : print("Download complete")
+yt = YouTube(link, on_progress_callback=f"{progress_func}%",
         on_complete_callback=complete_func,
-        proxies=my_proxies,
         )
 
 print(f"Title: {yt.title}")
