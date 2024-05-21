d0285205c94be152f884590be64ce8964bed9a1e
Man Njoro <elijohnmwoho@gmail.com>
2024-05-21 21:56:30 +0300

    Installed tailwind css

--- React-native/CrashCourse/app/index.jsx
+++ React-native/CrashCourse/app/index.jsx
@@ -1,22 +1,15 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href={"/profile"}>Go to profile</Link>
+      <Link href={"/profile"} style={{color: "blue"}}>Go to profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
