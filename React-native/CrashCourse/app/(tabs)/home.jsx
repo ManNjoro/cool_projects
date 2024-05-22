9d81a6a4016931559ac95ca08602aa96fc8ac5fe
Man Njoro <elijohnmwoho@gmail.com>
2024-05-22 23:03:03 +0300

    "Updated Home component: changed SafeAreaView style, commented out data array, and added ListEmptyComponent with EmptyState"

--- React-native/CrashCourse/app/(tabs)/home.jsx
+++ React-native/CrashCourse/app/(tabs)/home.jsx
@@ -4,12 +4,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }]}
+        // data={[{ id: 1 }, { id: 2 }]}
+        data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
@@ -39,6 +41,9 @@ const Home = () => {
               <Trending posts={[{id: 1}, {id:2}, {id:3}] ?? []} />
             </View>
           </View>
+        )}
+        ListEmptyComponent={()=>(
+         <EmptyState title="No videos Found" subtitle="Be the first one to upload the video" />
         )}
       />
     </SafeAreaView>
