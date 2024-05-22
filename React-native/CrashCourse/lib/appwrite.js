a7d6e8614b579c4b36698d499028e27e795a3cdc
Man Njoro <elijohnmwoho@gmail.com>
2024-05-22 20:15:41 +0300

    "Updated app.json and added UI components to sign-up.jsx"

---
+++ React-native/CrashCourse/lib/appwrite.js
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: "https://cloud.appwrite.io/v1",
+    platform: "com.jsm.aora",
+    projectId: "664d9264001c333602ec",
+    databaseId: "664d96bf00037fb48e13",
+    userCollectionId: "664d98a900057ef3a486",
+    videoCollectionId: "664d9a3b0029061339e0",
+    storageId: "664da0020003911f5f68"
+}
