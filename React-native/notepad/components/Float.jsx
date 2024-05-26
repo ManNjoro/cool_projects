f4c3d59ca49dfa81315d857661f5cebb4f0aac23
Man Njoro <elijohnmwoho@gmail.com>
2024-05-26 18:47:42 +0300

    "Refactor _layout.jsx: removed StatusBar and styles, added Stack from expo-router, and renamed App to RootLayout"

---
+++ React-native/notepad/components/Float.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+
+const Float = () => {
+  return (
+    <View>
+      <Text>Float</Text>
+      <Feather name='plus' />
+    </View>
+  )
+}
+
+export default Float
