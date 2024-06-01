fcf8a9497cca55cd2b3b4bcb1ccec6c6730fb547
Man Njoro <elijohnmwoho@gmail.com>
2024-06-01 21:43:19 +0300

    "Added settings button with tooltip, sidebar, navbar, and routes for various pages and apps"

--- React/dashboard/src/App.jsx
+++ React/dashboard/src/App.jsx
@@ -1,24 +1,66 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { FiSettings } from 'react-icons/fi'
+import { FiSettings } from "react-icons/fi";
 import "./App.css";
 
 function App() {
+  const activeMenu = false;
   return (
     <>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
           <div
-            className="fixed right-4 bottom-4"
-            style={{ zIndex: "1000" }}
-          ></div>
-          <TooltipComponent content={"Settings"} position="Top">
-            <button>
-              
-              <FiSettings />
-            </button>
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              Navbar
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element="Ecommerce" />
+              <Route path="/ecommerce" element="Ecommerce" />
+              {/* Pages */}
+              <Route path="/ordres" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
 
-          </TooltipComponent>
+              {/* Apps */}
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calendar" element="Calendar" />
+              <Route path="/color-picker" element="ColorPicker" />
+              {/* Charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="pyramid" />
+              <Route path="/stacked" element="Stacked" />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </>
