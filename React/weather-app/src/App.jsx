79335a74aab29dd62178ece17558aaf0432e71e6
ManNjoro <elijohnmwoho@gmail.com>
2024-07-10 16:54:36 +0300

    "Added axios dependency, updated package.json and package-lock.json, added CSS styles for glitch effect, and modified App.jsx to fetch weather data using axios and display loading animation"

--- React/weather-app/src/App.jsx
+++ React/weather-app/src/App.jsx
@@ -2,12 +2,43 @@ import { useState } from "react";
 import "./App.css";
 import Search from "./components/search/Search";
 import Weather from "./components/weather/Weather";
+import axios from "axios";
 
 function App() {
   const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(false)
-  const [weatherData, setWeatherData] = useState(null)
-  const handleSearch = async () => {};
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchWeatherData = async (param) => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${param}&appid=${
+          import.meta.env.VITE_API
+        }`
+      );
+      console.log("data", res.data);
+      setWeatherData(res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    fetchWeatherData(search);
+  };
+
+  if (loading) {
+    return (
+      <div className="loader">
+        <div data-glitch="Loading..." className="glitch">
+          Lo
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Search
