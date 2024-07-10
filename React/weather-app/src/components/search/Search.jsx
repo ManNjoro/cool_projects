c48abcdd0d89e8777c0f5cc80e2c23ccb182e9b5
ManNjoro <elijohnmwoho@gmail.com>
2024-07-10 16:30:38 +0300

    "Refactor App component, update CSS styles, and add Search and Weather components"

--- React/weather-app/src/components/search/Search.jsx
+++ React/weather-app/src/components/search/Search.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
 
-export default function Search() {
+export default function Search({ search, setSearch, handleSearch }) {
   return (
-    <div>Search</div>
-  )
+    <div className="search-engine">
+      <input
+        type="text"
+        placeholder="Search for a city"
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search weather</button>
+    </div>
+  );
 }
