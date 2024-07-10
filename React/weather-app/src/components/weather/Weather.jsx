9b3f5bc410dd581ab0d00e1abe008ac37e701175
ManNjoro <elijohnmwoho@gmail.com>
2024-07-10 19:20:02 +0300

    "Refactor App component, move search and weather data fetching to Weather component, and update styles in App.css"

--- React/weather-app/src/components/weather/Weather.jsx
+++ React/weather-app/src/components/weather/Weather.jsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Search from "../search/Search";
+import axios from "axios";
 
 export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchWeatherData = async (param) => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${
+          import.meta.env.VITE_API
+        }`
+      );
+      console.log("data", res.data);
+      setWeatherData(res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    fetchWeatherData(search);
+  };
+
+  const getCurrentDate = () => {
+    return new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  };
+
+  useEffect(() => {
+    fetchWeatherData("Nairobi");
+  }, []);
+
   return (
-    <div>Weather</div>
-  )
+    <div>
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <div className="loader">
+          <div data-glitch="Loading..." className="glitch"></div>
+        </div>
+      ) : (
+        <div>
+          <div className="city-name">
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div>{weatherData?.main?.temp}</div>
+          <p className="description">{weatherData?.weather[0].description}</p>
+          <div className="weather-info">
+            <div>
+              <div>
+                <p className="wind">{weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p className="humidity">{weatherData?.main?.humidity}</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
