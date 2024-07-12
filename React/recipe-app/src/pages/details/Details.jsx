437be187626011f190daa850cbf4fc11f64bb6fd
ManNjoro <elijohnmwoho@gmail.com>
2024-07-12 22:53:23 +0300

    "Updated Details and Home pages with new API call, component rendering, and styling changes"

--- React/recipe-app/src/pages/details/Details.jsx
+++ React/recipe-app/src/pages/details/Details.jsx
@@ -1,35 +1,68 @@
-import { useContext, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import { GlobalContext } from "../../context/GlobalState"
-import axios from "axios"
-
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalState";
+import axios from "axios";
 
 export default function Details() {
-  const {id} = useParams()
-  const {recipeDetailsData, setRecipeDetailsData, setError, error,} = useContext(GlobalContext)
-  
-  const getRecipeDetails = async() =>{
+  const { id } = useParams();
+  console.log("ID", id);
+  const { recipeDetailsData, setRecipeDetailsData, setError, error } =
+    useContext(GlobalContext);
+
+  const getRecipeDetails = async () => {
     try {
       const { data } = await axios.get(
-        `https://forkify-api.herokuapp.com/api/v2/recipes?${id}`
+        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
       );
       if (data?.data) {
         setRecipeDetailsData(data?.data);
-
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    getRecipeDetails()
-  }, [id])
+  console.log(recipeDetailsData);
+
+  useEffect(() => {
+    getRecipeDetails();
+  }, [id]);
   return (
     <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
       <div className="row-start-2 lg:row-start-auto">
-        <div className="h-96 overflow-hidden rounded-xl group"></div>
+        <div className="h-96 overflow-hidden rounded-xl group">
+          <img
+            src={recipeDetailsData?.recipe?.image_url}
+            className="w-full h-full object-cover block group-hover:scale-105 duration-300"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <span className="text-sm text-cyan-700 font-medium">
+          {recipeDetailsData?.recipe?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {recipeDetailsData?.recipe?.title}
+        </h3>
+        <div>
+          <button className="p-3 px-8 rounded-lg text-sm uppercase font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white">
+            Save as Favorites
+          </button>
+        </div>
+        <div>
+          <span className="text-2xl font-semibold text-black">Ingredients</span>
+          <ul className="flex flex-col gap-3">
+            {
+              recipeDetailsData?.recipe?.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  <span className="text-2xl font-semibold text-black">{ingredient.quantity} {ingredient.unit}</span>
+                  <span className="text-2xl font-semibold text-black">{ingredient.description}</span>
+                </li>
+                ))
+            }
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
