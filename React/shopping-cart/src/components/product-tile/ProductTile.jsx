fce8db582ac6a4f439df2efdfb72462f95eb1e0d
ManNjoro <elijohnmwoho@gmail.com>
2024-07-13 19:14:20 +0300

    "Added axios dependency, updated Header component with navigation links, and implemented product listing with loading indicator on Home page"

---
+++ React/shopping-cart/src/components/product-tile/ProductTile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductTile({product}) {
+  return (
+    <div>
+        <h1>Single Product Tile</h1>
+    </div>
+  )
+}
--- React/shopping-cart/src/pages/Home.jsx
+++ React/shopping-cart/src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+import ProductTile from "../components/product-tile/ProductTile";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchListOfProducts = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get("https://fakestoreapi.com/products");
+      if (res.data) setProducts(res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchListOfProducts();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles
+            height={"120"}
+            width={"120"}
+            color="rgb(127, 29, 29"
+            visible={true}
+          />
+        </div>
+      ) : 
+      <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+        {
+            products && products.length > 0 &&
+            products.map(productItem => <ProductTile key={productItem.id} product={productItem} />)
+        }
+      </div>
+      }
+    </div>
+  );
 }
