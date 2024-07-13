638665c396539dba0c66f337f659fee623777623
ManNjoro <elijohnmwoho@gmail.com>
2024-07-13 18:52:22 +0300

    "Added react-router-dom and updated App component to include routes and header"

--- React/shopping-cart/src/App.jsx
+++ React/shopping-cart/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Header from "./components/header/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+
   )
 }
