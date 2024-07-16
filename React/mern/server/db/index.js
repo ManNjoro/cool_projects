d287c3a4196980446cfe465ad0daab073d6b04f6
ManNjoro <elijohnmwoho@gmail.com>
2024-07-16 19:57:32 +0300

    "Updated MongoDB connection string and required db index file in server index file"

--- React/mern/server/db/index.js
+++ React/mern/server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    import.meta.env.VITE_CONN_STRING
+    "mongodb+srv://eligachago:MTccqnfA3ElBWrwK@cluster0.aupqylj.mongodb.net/"
   )
   .then(() => console.log("Connected mongo db"))
   .catch((e) => console.log(e));
