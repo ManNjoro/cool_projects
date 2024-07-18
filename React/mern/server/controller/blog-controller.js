f545a21fd1fb05fc8160e8fc9284e7da5a95bf86
ManNjoro <elijohnmwoho@gmail.com>
2024-07-18 20:09:29 +0300

    "Added fetchListOfBlogs function to blog-controller.js and updated formatting"

--- React/mern/server/controller/blog-controller.js
+++ React/mern/server/controller/blog-controller.js
@@ -1,2 +1,23 @@
-const mongoose = require('mongoose')
-const Blog = require('../model/Blog')
+const mongoose = require("mongoose");
+const Blog = require("../model/Blog");
+
+// fetch list ofblogs
+
+const fetchListOfBlogs = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+  if (!blogList) {
+    return res.status(404).json({ message: "No Blogs found" });
+  }
+
+  return res.status(200).json({ blogList });
+};
+
+// add a new blog
+// delete a blog
+// update a blog
