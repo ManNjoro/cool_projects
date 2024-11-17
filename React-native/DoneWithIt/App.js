292e22d236f955df3a30e17c36c5e2446dacf628
Man Njoro <elijohnmwoho@gmail.com>
2024-11-17 22:36:22 +0300

    Padding

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -25,22 +25,32 @@ export default function App() {
   console.log(Dimensions.get("screen"));
   const orientation = useDeviceOrientation();
   console.log(orientation);
-  
+
   return (
     // <WelcomeScreen />
-      // <ViewImageScreen />
+    // <ViewImageScreen />
     <SafeAreaView style={styles.container}>
-       <View  style={{
-      backgroundColor: 'dodgerblue',
-      width: 100,
-      height: 100,
-      shadowColor: 'gray',
-      shadowOffset: {width: 10, height: 10},
-      shadowOpacity: 1,
-      elevation: 20
-    }}>
-
-       </View>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "gray",
+          shadowOffset: { width: 10, height: 10 },
+          shadowOpacity: 1,
+          elevation: 20,
+          padding: 20,
+          paddingHorizontal: 10
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+        ></View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -53,6 +63,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
 });
