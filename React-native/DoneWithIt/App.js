acc0d4ec414461a777122a4a699ad626b57c2dc7
Man Njoro <elijohnmwoho@gmail.com>
2024-11-17 20:31:45 +0300

    Shadows

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -34,11 +34,10 @@ export default function App() {
       backgroundColor: 'dodgerblue',
       width: 100,
       height: 100,
-      borderWidth: 10,
-      borderColor: 'royalblue',
-      borderRadius: 50,
-      // borderTopWidth: 20,
-      // borderTopLeftRadius: 50
+      shadowColor: 'gray',
+      shadowOffset: {width: 10, height: 10},
+      shadowOpacity: 1,
+      elevation: 20
     }}>
 
        </View>
