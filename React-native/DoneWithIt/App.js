023ec23d61f4656aa562a14ecd93c679c61cde00
Man Njoro <elijohnmwoho@gmail.com>
2024-11-18 11:40:14 +0300

    Improing the ViewImageScreen

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -34,8 +34,8 @@ export default function App() {
   return (
     // <WelcomeScreen />
     // <Demo />
-    <ListingDetailsScreen />
-    // <ViewImageScreen />
+    // <ListingDetailsScreen />
+    <ViewImageScreen />
     // <SafeAreaView style={styles.container}>
     //   <View
     //     style={{
