33ac07a70a452ffdd5de678bb092a508e4cb3f0f
Man Njoro <elijohnmwoho@gmail.com>
2024-11-20 00:15:31 +0300

    Built the ErrorMessage component

--- React-native/DoneWithIt/app/screens/LoginScreen.js
+++ React-native/DoneWithIt/app/screens/LoginScreen.js
@@ -1,11 +1,11 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
+import { Image, StyleSheet } from "react-native";
 import { Formik } from "formik";
+import * as Yup from "yup";
+
 import Screen from "../components/Screen";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
-import * as Yup from "yup";
-import AppText from "../components/AppText";
+import ErrorMessage from "../components/ErrorMessage";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -32,7 +32,7 @@ export default function LoginScreen() {
               onChangeText={handleChange("email")}
               textContentType="emailAddress"
             />
-            <AppText style={{ color: "red" }}>{errors.email}</AppText>
+            <ErrorMessage error={errors.email} />
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -42,7 +42,7 @@ export default function LoginScreen() {
               secureTextEntry
               textContentType="password"
             />
-            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+            <ErrorMessage error={errors.password} />
             <AppButton title="Login" onPress={() => handleSubmit()} />
           </>
         )}
