4b1bb47bc327e2c7c71cd92e711ff250dc1adf80
Man Njoro <elijohnmwoho@gmail.com>
2024-11-19 19:23:28 +0300

    Built the Listings Screen

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -27,6 +27,7 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   // console.log("App executed");
@@ -42,7 +43,8 @@ export default function App() {
       {/* <ViewImageScreen /> */}
 
       {/* <MessagesScreen /> */}
-      <AccountScreen />
+      {/* <AccountScreen /> */}
+      <ListingsScreen />
     </GestureHandlerRootView>
     // <SafeAreaView style={styles.container}>
     //   <View
