5dc316c8e660325b9ed877b7ba7f3a4ac2bbdf3e
Man Njoro <elijohnmwoho@gmail.com>
2024-11-19 22:22:56 +0300

    Handling Selections

--- React-native/DoneWithIt/App.js
+++ React-native/DoneWithIt/App.js
@@ -31,6 +31,7 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import Screen from "./app/components/Screen";
+import { useState } from "react";
 
 const categories = [
   {
@@ -48,17 +49,20 @@ const categories = [
 ];
 
 export default function App() {
-  // console.log("App executed");
-  // console.log(Dimensions.get("screen"));
-  // const orientation = useDeviceOrientation();
-  // console.log(orientation);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Screen>
         {/* <WelcomeScreen /> */}
         {/* <Demo /> */}
-        <AppPicker items={categories} icon="apps" placeholder="Category" />
+        <AppPicker
+          selectedItem={category}
+          onSelectItem={item => setCategory(item)}
+          items={categories}
+          icon="apps"
+          placeholder="Category"
+        />
         <AppTextInput icon="email" placeholder="Email" />
         {/* <ListingDetailsScreen /> */}
         {/* <ViewImageScreen /> */}
--- React-native/DoneWithIt/app/components/AppPicker.js
+++ React-native/DoneWithIt/app/components/AppPicker.js
@@ -15,7 +15,7 @@ import defaultStyles from "../config/styles";
 import AppText from "./AppText";
 import PickerItem from "./PickerItem";
 
-export default function AppPicker({ icon, items, placeholder }) {
+export default function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
   const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
@@ -29,7 +29,7 @@ export default function AppPicker({ icon, items, placeholder }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>{placeholder}</AppText>
+          <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -43,7 +43,10 @@ export default function AppPicker({ icon, items, placeholder }) {
           data={items}
           keyExtractor={(item) => item.value.toString()}
           renderItem={({ item }) => (
-            <PickerItem label={item.label} onPress={() => console.log(item)} />
+            <PickerItem label={item.label} onPress={() => {
+                setModalVisible(false);
+                onSelectItem(item);
+            }} />
           )}
         />
       </Modal>
