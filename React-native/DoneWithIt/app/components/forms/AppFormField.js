55743f40b49b30e710050895bfe0ae4b56083a65
Man Njoro <elijohnmwoho@gmail.com>
2024-11-20 23:09:57 +0300

    "Deleted AppForm, AppFormField, ErrorMessage, and SubmitButton components; removed imports and usages from LoginScreen"

--- React-native/DoneWithIt/app/components/forms/AppFormField.js
+++ React-native/DoneWithIt/app/components/forms/AppFormField.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import AppTextInput from "./AppTextInput";
+import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 import { useFormikContext } from "formik";
 
 export default function AppFormField({ name, ...otherProps }) {
-    const {errors, handleBlur, handleChange, touched} = useFormikContext()
+  const { errors, handleBlur, handleChange, touched } = useFormikContext();
   return (
     <>
       <AppTextInput
