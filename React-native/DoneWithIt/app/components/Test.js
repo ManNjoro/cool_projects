bdcd58b1432b5f30e6495713f7f5a74e5bfa1110
Man Njoro <elijohnmwoho@gmail.com>
2024-11-22 13:00:16 +0300

    Custom snippets

---
+++ React-native/DoneWithIt/app/components/Test.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function Test() {
+return (
+<View style={container}></View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {}
+});
