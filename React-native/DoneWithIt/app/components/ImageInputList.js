a3e8d5233603b7a724a5def8fa159c2a9dec1676
Man Njoro <elijohnmwoho@gmail.com>
2024-11-29 02:18:47 +0300

    "Added ScrollView to ImageInputList component, wrapped ImageInput elements, and added ref to scroll to end on content size change."

--- React-native/DoneWithIt/app/components/ImageInputList.js
+++ React-native/DoneWithIt/app/components/ImageInputList.js
@@ -1,21 +1,32 @@
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import ImageInput from "./ImageInput";
+import { useRef } from "react";
 
 export default function ImageInputList({
   imageUris = [],
   onRemoveImage,
   onAddImage,
 }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <ImageInput
-          imageUri={uri}
-          key={uri}
-          onChangeImage={() => onRemoveImage(uri)}
-        />
-      ))}
-      <ImageInput onChangeImage={uri=>onAddImage(uri)} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <ImageInput
+              imageUri={uri}
+              key={uri}
+              onChangeImage={() => onRemoveImage(uri)}
+            />
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
