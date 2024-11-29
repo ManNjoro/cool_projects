eabdb6a122c83ba5e439e3bf2466ff6ff6ce4cef
Man Njoro <elijohnmwoho@gmail.com>
2024-11-29 13:08:25 +0300

    Navigating Between Screens

--- React-native/DoneWithIt/app/components/Demo.js
+++ React-native/DoneWithIt/app/components/Demo.js
@@ -1,16 +1,47 @@
-import { Button, Image, StyleSheet } from "react-native";
+import { Button, Image, StyleSheet, Text } from "react-native";
 import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import Screen from "./Screen";
 import ImageInput from "./ImageInput";
 import ImageInputList from "./ImageInputList";
 import ListingEditScreen from "../screens/ListingEditScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import AppButton from "./AppButton";
+
+const Link = () => {
+  const navigation = useNavigation()
+  return (
+    <AppButton title="Click" onPress={()=> navigation.navigate('TweetDetails')} />
+  )
+}
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+);
+ 
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweets</Text>    
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function Demo() {
   return (
-    <Screen>
-      <ListingEditScreen />
-    </Screen>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
