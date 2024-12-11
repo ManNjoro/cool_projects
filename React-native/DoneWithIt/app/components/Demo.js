dfdae7344783be1c0c2f9606bcd1fb62ccaf1376
Man Njoro <elijohnmwoho@gmail.com>
2024-12-11 13:57:20 +0300

    `Refactor Demo component to use @react-native-community/netinfo`

--- React-native/DoneWithIt/app/components/Demo.js
+++ React-native/DoneWithIt/app/components/Demo.js
@@ -1,103 +1,13 @@
-import { Button, Image, StyleSheet, Text } from "react-native";
-import { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-import Screen from "./Screen";
-import ImageInput from "./ImageInput";
-import ImageInputList from "./ImageInputList";
-import ListingEditScreen from "../screens/ListingEditScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import AppButton from "./AppButton";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <AppButton
-      title="Click"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-
-const Tweets = () => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Detail {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createNativeStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: "dodgerblue" },
-      headerTintColor: "white",
-      headerShown: true
-    }}
-  >
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetDetails}
-      options={({ route }) => ({ title: route.params.id.toString() })}
-    />
-  </Stack.Navigator>
-);
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarActiveBackgroundColor: "tomato",
-      tabBarActiveTintColor: "white",
-      tabBarInactiveBackgroundColor: "#eee",
-      tabBarInactiveTintColor: "black",
-      headerShown: false
-    }}
-  >
-    <Tab.Screen
-      name="Feed"
-      component={FeedNavigator}
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name="home" size={size} color={color} />
-        ),
-      }}
-    />
-    <Tab.Screen name="Accounts" component={Account} />
-  </Tab.Navigator>
-);
+import { Button, StyleSheet } from "react-native";
+import NetInfo, {useNetInfo} from '@react-native-community/netinfo'
 
 export default function Demo() {
+  const netInfo = useNetInfo()
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <Button disabled={!netInfo.isInternetReachable} title="Switch" />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#f8f4f4",
-    padding: 20,
-    paddingTop: 100,
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
+  
 });
