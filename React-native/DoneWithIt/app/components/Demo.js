4de344ff9f460f2222dced6940fc12b9e40aa52e
Man Njoro <elijohnmwoho@gmail.com>
2024-12-11 14:35:11 +0300

    Implementing a cache layer

--- React-native/DoneWithIt/app/components/Demo.js
+++ React-native/DoneWithIt/app/components/Demo.js
@@ -1,10 +1,21 @@
 import { Button, StyleSheet } from "react-native";
 import NetInfo, {useNetInfo} from '@react-native-community/netinfo'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Demo() {
-  const netInfo = useNetInfo()
+  const storeData = async () => {
+    try{
+      await AsyncStorage.setItem('person', JSON.stringify({id: 1}))
+      const value = await AsyncStorage.getItem('person')
+      const person = JSON.parse(value)
+      console.log(person)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  storeData()
   return (
-    <Button disabled={!netInfo.isInternetReachable} title="Switch" />
+    null
   );
 }
 
