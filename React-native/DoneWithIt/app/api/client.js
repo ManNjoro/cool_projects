c01b5c664e1c1fe0fe0711308e0dc4b6608d27a2
Man Njoro <elijohnmwoho@gmail.com>
2024-12-15 00:32:34 +0300

    Calling Protected Apis

--- React-native/DoneWithIt/app/api/client.js
+++ React-native/DoneWithIt/app/api/client.js
@@ -1,9 +1,16 @@
 import { create } from "apisauce";
 import cache from '../utility/cache'
+import authStorage from '../auth/storage'
 
 const apiClient = create({
   baseURL: "http://192.168.100.11:9000/api",
 });
+
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken()
+  if(!authToken) return
+  request.headers['x-auth-token'] = authToken
+})
 
 // We are changing the implementation of the get function so that
 // if one is offline the data is fetched from the cache
