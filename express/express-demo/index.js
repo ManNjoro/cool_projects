f92b93dc50df44a8a415d13468ca83302f2fb31e
Man Njoro <elijohnmwoho@gmail.com>
2025-01-22 15:29:43 +0300

    Initial commit

--- express/express-demo/index.js
+++ express/express-demo/index.js
@@ -29,9 +29,9 @@ app.get("/api/courses", (req, res) => {
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
 
-  if (error) {
-    res.status(400).send(error.details[0].message);
-  }
+  if (error)
+    return res.status(400).send(error.details[0].message);
+  
 
   const course = {
     id: courses.length + 1,
@@ -44,16 +44,25 @@ app.post("/api/courses", (req, res) => {
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res.status(404).send("The course with the given ID was not found");
 
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-  }
+  if (error) 
+    return res.status(400).send(error.details[0].message);
   
+
   course.name = req.body.name;
   res.send(course);
 });
+
+app.delete('/api/courses/:id', (req, res)=>{
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found");
+  const index = courses.indexOf(course)
+  courses.splice(index, 1)
+  res.send(course)
+})
 
 function validateCourse(course) {
   const schema = Joi.object({
@@ -72,6 +81,9 @@ app.get("/api/courses/:id", (req, res) => {
 app.get("/api/posts/:year/:month", (req, res) => {
   res.send(req.query);
 });
+
+
+
 
 const port = process.env.PORT || 3000;
 
