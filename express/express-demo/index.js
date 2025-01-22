804a6e6bd5d5e842bd6b4eb15240c9472c35863a
Man Njoro <elijohnmwoho@gmail.com>
2025-01-22 15:09:09 +0300

    Joi validation

--- express/express-demo/index.js
+++ express/express-demo/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
+import Joi from 'joi'
+import express, { json } from 'express'
 const app = express()
+app.use(json())
 
 const courses = [
     {
@@ -24,11 +26,30 @@ app.get('/api/courses', (req, res) => {
     res.send(courses)
 })
 
+app.post('/api/courses', (req, res)=>{
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    const result = schema.validate(req.body)
+    
+    if(result.error){
+        res.status(400).send(result.error.details[0].message)
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name,
+    }
+    courses.push(course)
+    res.send(course)
+})
+
 app.get('/api/courses/:id', (req, res)=>{
     const course = courses.find((c)=> c.id === parseInt(req.params.id))
     if (!course) res.status(404).send("The course with the given ID was not found")
     res.send(course)
 })
+
 app.get('/api/posts/:year/:month', (req, res)=>{
     res.send(req.query)
 })
