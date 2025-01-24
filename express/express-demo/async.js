8a842cf1bc109fc9a8245acf2f0e61eb63988317
Man Njoro <elijohnmwoho@gmail.com>
2025-01-24 18:20:03 +0300

    Refactor async functions to use Promises instead of callbacks

--- express/express-demo/async.js
+++ express/express-demo/async.js
@@ -1,7 +1,13 @@
 console.log('Before');
-getUser(1, getRepositories)
+// getUser(1, getRepositories)
 
 console.log('After');
+
+getUser(1)
+.then(user=>getRepositories(user.gitHubUsername))
+.then(repos => getCommits(repos[0]))
+.then(commits => console.log('Commits', commits))
+.catch(err => console.log('Error', err.message))
 
 function getRepositories(user) {
     getRepositories(user.gitHubUsername, getCommits);
@@ -13,20 +19,29 @@ function displayCommits(commits){
 }
 
 function getCommits(repos){
-    getCommits(repos, displayCommits)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting commits for repo...', repos);
+            resolve(['commit1', 'commit2', 'commit3'])
+        }, 2000);
+    })
 }
 
-function getUser(id, callback) {
-    setTimeout(()=>{
-        console.log('Reading a user from database...'); 
-        callback({id: id, gitHubUsername: 'Eli'})
-    }, 2000)
-    return 1
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Reading a user from database...'); 
+            resolve({id: id, gitHubUsername: 'Eli'})
+        }, 2000)
+
+    })
 }
 
-function getRepositories(username, callback) {
-    setTimeout(()=>{
-        console.log('Fetching repositories for:', username);
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000)
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Fetching repositories for:', username);
+            resolve(['repo1', 'repo2', 'repo3'])
+        }, 2000)
+    })
 }
