ec67e4c94d937a67ffc633724a8e477f006ef858
Man Njoro <elijohnmwoho@gmail.com>
2025-01-24 17:44:33 +0300

    Refactor async code in async.js to use callbacks

--- express/express-demo/async.js
+++ express/express-demo/async.js
@@ -1,6 +1,24 @@
 console.log('Before');
-setTimeout(()=>{
-    console.log('Reading a user from database...');
-    
-}, 2000)
+getUser(1, (user) => {
+    console.log('User:', user);
+    getRepositories(user.gitHubUsername, (repos) => {
+        console.log('Repositories:', repos);
+    });
+})
+
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(()=>{
+        console.log('Reading a user from database...'); 
+        callback({id: id, gitHubUsername: 'Eli'})
+    }, 2000)
+    return 1
+}
+
+function getRepositories(username, callback) {
+    setTimeout(()=>{
+        console.log('Fetching repositories for:', username);
+        callback(['repo1', 'repo2', 'repo3'])
+    }, 2000)
+}
