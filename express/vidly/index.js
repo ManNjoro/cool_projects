a574c807308dfc6c31e071855757fc07a11a883f
Man Njoro <elijohnmwoho@gmail.com>
2025-01-26 16:06:01 +0300

    Added routes for movies and updated index.js to include them

--- express/vidly/index.js
+++ express/vidly/index.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const express = require("express");
 const app = express();
 const genres = require('./routes/genres')
+const movies = require('./routes/movies')
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to mongodb...'))
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres)
+app.use('/api/movies', movies)
 
 const port = process.env.PORT || 3000
 
