5e70a3c3702965d37dbe5cdddc095f21283ec0fc
Man Njoro <elijohnmwoho@gmail.com>
2025-02-04 02:08:07 +0300

    Using lodash

--- express/vidly/routes/users.js
+++ express/vidly/routes/users.js
@@ -1,55 +1,22 @@
-const mongoose = require('express')
+const _ = require('lodash')
 const express = require("express");
 const Joi = require("joi");
 const { User, validate } = require('../models/user');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const genres = await Genre.find().sort("name");
-  res.send(genres);
-});
-
-router.get("/:id", async (req, res) => {
-  const genre = await User.findById(req.params.id);
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-  res.send(genre);
-});
-
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+
+  let user = await User.findOne({email: req.body.email})
+  if (user) return res.status(400).send('User already registered.')
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
   await user.save();
+  
 
-  res.send(user);
+  res.send(_.pick(user, ['_id','name', 'email']));
 });
 
-router.put("/:id", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const genre = await Genre.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.name },
-    { new: true }
-  );
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  res.send(genre);
-});
-
-router.delete("/:id", async (req, res) => {
-  const genre = await Genre.findByIdAndDelete(req.params.id);
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  res.send(genre);
-});
 
 module.exports = router;
 
