55b6d1dc4b37c424d3bbcd1597bb05ba0253f46f
Man Njoro <elijohnmwoho@gmail.com>
2025-02-17 00:00:17 +0300

    Removed unnecessary dependencies and logging configurations

--- express/vidly/index.js
+++ express/vidly/index.js
@@ -1,35 +1,14 @@
-require('express-async-errors')
-const winston = require('winston')
-require('winston-mongodb')
 const config = require('config')
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid')(Joi)
 
 const express = require("express");
 const app = express();
+require('./startup/logging')()
 require('./startup/routes')(app)
 require('./startup/db')()
 
-winston.add(new winston.transports.File({
-  filename: 'uncaughtExceptions.log',
-  handleExceptions: true,
-  handleRejections: true
-}))
 
-// process.on('uncaughtRejection', (ex) => {
-//   winston.error(ex.message, ex)
-//   process.exit(1)
-// })
-
-winston.add(new winston.transports.File({filename: 'logfile.log'}))
-// winston.configure({
-//   transports: [new winston.transports.File({filename: 'logfile.log'})],
-//   format: winston.format.combine(
-//     winston.format.timestamp(),
-//     winston.format.json()
-//   )
-// })
-winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly'}))
 
 if (!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey is not defined')
---
+++ express/vidly/startup/logging.js
@@ -0,0 +1,22 @@
+const winston = require('winston')
+require('winston-mongodb')
+require('express-async-errors')
+
+module.exports = function() {
+    winston.add(new winston.transports.File({
+        filename: 'uncaughtExceptions.log',
+        handleExceptions: true,
+        handleRejections: true
+      }))
+      
+      
+      winston.add(new winston.transports.File({filename: 'logfile.log'}))
+      // winston.configure({
+      //   transports: [new winston.transports.File({filename: 'logfile.log'})],
+      //   format: winston.format.combine(
+      //     winston.format.timestamp(),
+      //     winston.format.json()
+      //   )
+      // })
+      winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly'}))
+}
