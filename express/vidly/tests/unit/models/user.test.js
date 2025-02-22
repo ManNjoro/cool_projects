7bb24973acc8c5c73f8f474dbd4a57bbf72bf62d
Man Njoro <elijohnmwoho@gmail.com>
2025-02-22 16:44:49 +0300

    Installation of jest

---
+++ express/vidly/tests/unit/models/user.test.js
@@ -0,0 +1,10 @@
+const {User} = require('../../../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+describe('user.generateAuthToken', () => {
+    it('should return a valid JWT', () => {
+        const user = new User({_id: 1, isAdmin: true})
+        const token = user.generateAuthToken()
+    })
+})
