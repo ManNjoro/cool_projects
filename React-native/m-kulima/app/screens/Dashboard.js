b0a0cb4912ee7d2f161206452e90944662d7a5b1
Man Njoro <elijohnmwoho@gmail.com>
2025-04-30 19:14:19 +0300

    Added styles in Dashboard screen

--- React-native/m-kulima/app/screens/Dashboard.js
+++ React-native/m-kulima/app/screens/Dashboard.js
@@ -1,7 +1,28 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Screen from "../components/Screen";
 
 export default function Dashboard() {
-    return <View style={styles.container}>
-        
-    </View>
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <Text style={styles.title}>Total cows</Text>
+        <Text style={styles.subTitle}>5</Text>
+      </View>
+    </Screen>
+  );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 10,
+        display: flex,
+        flexDirection: "column"
+    },
+    title: {
+        fontSize: 16
+    },
+    subTitle: {
+        fontSize: 10
+    }
+})
