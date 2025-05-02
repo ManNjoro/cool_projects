190a4afa6b84e8c583793bebc169e8b60a587d62
Man Njoro <elijohnmwoho@gmail.com>
2025-05-03 00:50:33 +0300

    Removed paddingTop from Screen component styles

--- React-native/m-kulima/app/components/Screen.js
+++ React-native/m-kulima/app/components/Screen.js
@@ -12,7 +12,7 @@ export default function Screen({ children, style }) {
 
 const styles = StyleSheet.create({
   screen: {
-    paddingTop: Constants.statusBarHeight,
+    // paddingTop: Constants.statusBarHeight,
     flex: 1,
   },
   content: {
